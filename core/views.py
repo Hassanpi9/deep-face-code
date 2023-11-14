fdc453d0a4740101548e55bbf749a1936ef30fd8
Hassan Abdul Basit <98455659+Hassanpi9@users.noreply.github.com>
2023-11-14 21:32:10 +0500

    ok

--- core/views.py
+++ core/views.py
@@ -3,9 +3,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import UploadedImage
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
 from .serializers import ImageSerializer
 import cv2
 from deepface import DeepFace
@@ -21,21 +18,8 @@ class ImageAnalysisAPIView(APIView):
             # Perform analysis using DeepFace
             image_path = image.temporary_file_path()
             image_data = cv2.imread(image_path)
-            result = DeepFace.analyze(image_data, actions=["emotion"])
-            emotion = result[0]["dominant_emotion"]
+            result = DeepFace.analyze(image_data, actions=["emotion"])            
 
-            # Update the image with the detected emotion
-            cv2.putText(image_data, emotion, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-             # Save the updated image in the media directory using Django's File class
-            processed_image_path = os.path.join('media', image.name)
-            processed_image_content = cv2.imencode('.jpg', image_data)[1].tostring()
-            processed_image = ContentFile(processed_image_content)
-
-            with default_storage.open(processed_image_path, 'wb') as destination:
-                destination.write(processed_image.read())
-            
-
-            return Response({'result': emotion,'processed_image_path': processed_image_path}, status=status.HTTP_200_OK)
+            return Response({'result': result}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
